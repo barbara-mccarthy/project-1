0b2b508e182e7290e19505e27dd658a157214698
Barbara McCarthy <barbara_mccarthy@harvard.edu>
2014-09-18 09:23:54 -0400

    added index file

---
+++ index.php
@@ -0,0 +1,7 @@
+<div>
+	<div>In 2+ sentences describe who you are and what you do.</div>
+	<div>Why are you taking this course?</div>
+	<div>What previous experience do you have with programming?</div>
+	<div>Are you working on a Mac, PC, or some other platform?</div>
+	<div>Anything else we should know?</div>
+</div>
